a26abb1c1d7ea454862373ef0315914aa099cbcf
Artur <arturggerhardt@gmail.com>
2024-07-19 21:14:03 -0300

    Litfunc

--- PublicandoStreamlit.py
+++ PublicandoStreamlit.py
@@ -12,9 +12,9 @@ model = tf.keras.models.load_model('./models/ceMod2.h5')
 classes = ['Raiva', 'Alegria', 'Neutro', 'Triste', 'Surpresa']
 
 # Função para realizar a previsão 
-def predict_image(file_path):
-    img = image.load_img(file_path, target_size=(200, 200))
-    img = image.img_to_array(img)
+def predict_image(imagem):
+    #img = image.load_img(imagem, target_size=(200, 200))
+    img = image.img_to_array(imagem)
     img = np.expand_dims(img, axis=0)
     img = img / 255.0
 
@@ -32,16 +32,15 @@ uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "
 
 if uploaded_file is not None:
     imagem = Image.open(uploaded_file)
-    st.image(image, caption='Imagem carregada.', use_column_width=True)
+    st.image(imagem, caption='Imagem carregada.', use_column_width=True)
     st.write("")
-    st.write("Classificando...")
 
     predictions, emocao = predict_image(imagem)
 
     st.write(f'A emoção classificada é: {emocao}')
     st.write('Resultados:')
-    st.write(f'Raiva: {round(predictions[0]*100,2)}%')
-    st.write(f'Alegria: {round(predictions[1]*100,2)}%')
-    st.write(f'Neutro: {round(predictions[2]*100,2)}%')
-    st.write(f'Triste: {round(predictions[3]*100,2)}%')
-    st.write(f'Surpresa: {round(predictions[4]*100,2)}%')
+    st.write(f'Raiva: {predictions[0][0]*100:.2f}%')
+    st.write(f'Alegria: {predictions[0][1]*100:.2f}%')
+    st.write(f'Neutro: {predictions[0][2]*100:.2f}%')
+    st.write(f'Triste: {predictions[0][3]*100:.2f}%')
+    st.write(f'Surpresa: {predictions[0][4]*100:.2f}%')
